72afa8713176bce8dfe8fe3f2a46c7372d46242a
Luigi Marini <lmarini@illinois.edu>
2016-01-08 15:56:24 -0600

    Simple proof of concept using Finatra instead of Finagle.
    I am hoping this will make the development easier, but I am starting to think that it will complicate things more. This is to help compare the two approaches to pick which ever one is best.

--- build.sbt
+++ build.sbt
@@ -4,7 +4,18 @@ version := "0.1.0"
 
 scalaVersion := "2.11.7"
 
-resolvers += Resolver.sonatypeRepo("snapshots")
+lazy val versions = new {
+  val finatra = "2.1.2"
+  val guice = "4.0"
+  val logback = "1.0.13"
+}
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("releases"),
+  Resolver.sonatypeRepo("snapshots"),
+  "Twitter Maven" at "https://maven.twttr.com"
+)
+
 
 libraryDependencies ++= Seq(
   "com.twitter" %% "finagle-core" % "6.31.0",
@@ -12,8 +23,36 @@ libraryDependencies ++= Seq(
   "com.twitter" %% "finagle-stats" % "6.31.0",
   "com.twitter" %% "finagle-redis" % "6.31.0",
   "com.twitter" %% "twitter-server" % "1.16.0",
-  "com.typesafe" % "config" % "1.3.0"
-//  "com.github.finagle" %% "finch-core" % "0.9.2",
+  "com.typesafe" % "config" % "1.3.0",
+
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra,
+  "com.twitter.finatra" %% "finatra-httpclient" % versions.finatra,
+  "com.twitter.finatra" %% "finatra-slf4j" % versions.finatra,
+  "com.twitter.inject" %% "inject-core" % versions.finatra,
+  "ch.qos.logback" % "logback-classic" % versions.logback,
+
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra % "test",
+  "com.twitter.finatra" %% "finatra-jackson" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-server" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-app" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-core" % versions.finatra % "test",
+  "com.twitter.inject" %% "inject-modules" % versions.finatra % "test",
+  "com.google.inject.extensions" % "guice-testlib" % versions.guice % "test",
+
+  "com.twitter.finatra" %% "finatra-http" % versions.finatra % "test" classifier "tests",
+  "com.twitter.finatra" %% "finatra-jackson" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-app" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-core" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-modules" % versions.finatra % "test" classifier "tests",
+  "com.twitter.inject" %% "inject-server" % versions.finatra % "test" classifier "tests",
+
+
+  "org.mockito" % "mockito-core" % "1.9.5" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.3" % "test",
+  "org.specs2" %% "specs2" % "2.3.12" % "test"
+
+
+  //  "com.github.finagle" %% "finch-core" % "0.9.2",
 //  "com.github.finagle" %% "finch-circe" % "0.9.2",
 //  "io.circe" %% "circe-generic" % "0.2.1"
 )
---
+++ src/main/scala/FinatraServer.scala
@@ -0,0 +1,136 @@
+import javax.inject.{Inject, Singleton}
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import com.google.inject.Provides
+import com.twitter.finagle.Http
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.{Controller, HttpServer}
+import com.twitter.finatra.httpclient.modules.HttpClientModule
+import com.twitter.finatra.httpclient.{HttpClient, RequestBuilder}
+import com.twitter.finatra.json.FinatraObjectMapper
+import com.twitter.inject.TwitterModule
+import com.twitter.util.Base64StringEncoder
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by lmarini on 1/5/16.
+  */
+object FinatraServerMain extends FinatraServer
+
+class FinatraServer extends HttpServer {
+
+  override val modules = Seq(MyModule1, DAPHttpClientModule)
+
+  override def configureHttp(router: HttpRouter) {
+    router
+      .add[ExampleController]
+      .add[HelloWorldController]
+      .add[DAPController]
+      .add[DTSController]
+  }
+}
+
+object DAPHttpClientModule extends HttpClientModule {
+  private val conf = ConfigFactory.load()
+  override val dest = conf.getString("dap.url")
+  override val hostname = "localhost"
+  val user = conf.getString("dap.user")
+  val password = conf.getString("dap.password")
+  val encodedCredentials = "Basic " + Base64StringEncoder.encode(s"$user:$password".getBytes)
+  override val defaultHeaders = Map("Authorization" -> encodedCredentials)
+}
+
+object MyObjectMapper extends ObjectMapper with ScalaObjectMapper
+
+@Singleton
+class DAPController @Inject()(httpClient: HttpClient) extends Controller {
+  private val conf = ConfigFactory.load()
+  get("/dap/:*") { request: Request =>
+    val dapReq = RequestBuilder.get("/" + request.params("*"))
+    val user = conf.getString("dap.user")
+    val password = conf.getString("dap.password")
+    val encodedCredentials = "Basic " + Base64StringEncoder.encode(s"$user:$password".getBytes)
+
+    val dap = new HttpClient(
+      hostname = "localhost",
+      httpService = Http.newService(conf.getString("dap.url")),
+      retryPolicy = None,
+      defaultHeaders = Map("Authorization" -> encodedCredentials),
+      mapper = new FinatraObjectMapper(MyObjectMapper))
+
+    dap.execute(dapReq)
+  }
+}
+
+@Singleton
+class DTSController extends Controller {
+  private val conf = ConfigFactory.load()
+
+  post("/dts/:*") { request: Request =>
+    val dapReq = RequestBuilder.post("/" + request.params("*"))
+    val user = conf.getString("dap.user")
+    val password = conf.getString("dap.password")
+    val encodedCredentials = "Basic " + Base64StringEncoder.encode(s"$user:$password".getBytes)
+    val dap = new HttpClient(
+      hostname = "localhost",
+      httpService = Http.newService(conf.getString("dap.url")),
+      retryPolicy = None,
+      defaultHeaders = Map("Authorization" -> encodedCredentials),
+      mapper = new FinatraObjectMapper(MyObjectMapper))
+    dap.execute(dapReq)
+  }
+  get("/dts/:*") { request: Request =>
+    request.params("*")
+    val dapReq = RequestBuilder.get("/" + request.params("*"))
+    val user = conf.getString("dts.user")
+    val password = conf.getString("dts.password")
+    val encodedCredentials = "Basic " + Base64StringEncoder.encode(s"$user:$password".getBytes)
+    val dts = new HttpClient(
+      hostname = "localhost",
+      httpService = Http.newService(conf.getString("dts.url")),
+      retryPolicy = None,
+      defaultHeaders = Map("Authorization" -> encodedCredentials),
+      mapper = new FinatraObjectMapper(MyObjectMapper))
+    dts.execute(dapReq)
+  }
+}
+
+class ExampleController @Inject()(
+  exampleService: ExampleService
+) extends Controller {
+
+  get("/ping") { request: Request =>
+    exampleService.pong()
+  }
+}
+
+class HelloWorldController extends Controller {
+
+  get("/hi") { request: Request =>
+    info("hi")
+    "Hello " + request.params.getOrElse("name", "unnamed")
+  }
+
+  post("/hi") { hiRequest: HiRequest =>
+    "Hello " + hiRequest.name + " with id " + hiRequest.id
+  }
+}
+
+case class HiRequest(
+  id: Long,
+  name: String)
+
+object MyModule1 extends TwitterModule {
+  val key = flag("key", "defaultkey", "The key to use.")
+
+  @Provides
+  def providesExampleService: ExampleService = {
+    new ExampleService()
+  }
+}
+
+class ExampleService() {
+  def pong() = "pong"
+}
