ab3f5b8d00abc71fe5981bb981e6bbd0456899c8
Luigi Marini <lmarini@illinois.edu>
2016-11-07 15:08:05 -0600

    Set BD-Host and BD-Access-Token for backend services where appropriate

---
+++ src/main/scala/edu/illinois/ncsa/fence/util/GatewayHeaders.scala
@@ -0,0 +1,30 @@
+package edu.illinois.ncsa.fence.util
+
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.util.Future
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Store and manipulate Gateway headers.
+  */
+object GatewayHeaders {
+
+  /** Load configuration file using typesafehub config */
+  private val conf = ConfigFactory.load()
+
+  // TODO Change ot BD-Username
+  val usernameHeader = "X-BD-Username"
+
+  val gatewayHostHeader = "BD-Host"
+
+  val tokenHeader = "BD-Access-Token"
+
+  class GatewayHostHeaderFilter() extends SimpleFilter[Request, Response] {
+    override def apply(request: Request, continue: Service[Request, Response]): Future[Response] = {
+      conf.getString("auth.provider")
+      request.headerMap.add(gatewayHostHeader, conf.getString("fence.hostname"))
+      continue(request)
+    }
+  }
+}
