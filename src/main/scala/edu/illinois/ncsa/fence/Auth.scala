45042246c0b2d9645dcce177c0e228240175af07
Luigi Marini <lmarini@illinois.edu>
2017-02-06 10:28:03 -0600

    Fixed accidental bug introduced by renaming the functions to increase/decrease stat counters.

--- src/main/scala/edu/illinois/ncsa/fence/Auth.scala
+++ src/main/scala/edu/illinois/ncsa/fence/Auth.scala
@@ -29,7 +29,7 @@ object Auth {
           case Some(buf) => {
             val keyFromRedis = BufToString(buf)
             val token = Redis.createToken(key)
-            Redis.decreaseStat("tokens")
+            Redis.increaseStat("tokens")
             val res = Response(req.version, Status.Ok)
             res.contentType = "application/json;charset=UTF-8"
             res.content = Buf.Utf8(JsonConverter.writeToString(Map("token"->token)))
@@ -89,7 +89,7 @@ object Auth {
       BasicAuth.extractCredentials(req) match {
         case Some(cred) =>
           val apiKey = Redis.createApiKey(cred.username)
-          Redis.decreaseStat("keys")
+          Redis.increaseStat("keys")
           val res = Response(req.version, Status.Ok)
           res.contentType = "application/json;charset=UTF-8"
           res.content = Buf.Utf8(JsonConverter.writeToString(Map("api-key"->apiKey)))
