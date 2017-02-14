c81d28183eff874654cd08c63ac87f4532184317
Luigi Marini <lmarini@illinois.edu>
2017-02-14 14:39:39 -0600

    Simple implementation of a rate limiting using buckets. How many messages per interval and size of interval in seconds can be set in the config file.

--- src/main/scala/edu/illinois/ncsa/fence/Quotas.scala
+++ src/main/scala/edu/illinois/ncsa/fence/Quotas.scala
@@ -3,12 +3,15 @@ package edu.illinois.ncsa.fence
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.finagle.http.{Request, Response, Status, Version}
 import com.twitter.util.Future
+import com.typesafe.config.ConfigFactory
 import edu.illinois.ncsa.fence.util.GatewayHeaders
 
 /**
   * Manage user quotas.
   */
 object Quotas {
+
+  private val conf = ConfigFactory.load()
 
   class RequestsQuotasFilter() extends SimpleFilter[Request, Response] {
     override def apply(request: Request, continue: Service[Request, Response]): Future[Response] = {
@@ -20,6 +23,28 @@ object Quotas {
             } else {
               val error = Response(Version.Http11, Status.Forbidden)
               error.setContentString("Requests quotas depleted")
+              Future(error)
+            }
+          }
+        case None =>
+          val error = Response(Version.Http11, Status.InternalServerError)
+          error.setContentString("User not found in request")
+          Future(error)
+      }
+    }
+  }
+
+  class RateLimitFilter() extends SimpleFilter[Request, Response] {
+    override def apply(request: Request, continue: Service[Request, Response]): Future[Response] = {
+      request.headerMap.get(GatewayHeaders.usernameHeader) match {
+        case Some(user) =>
+          Redis.checkRateLimit(user) flatMap { valid =>
+            if (valid) {
+              continue(request)
+            } else {
+              val seconds = conf.getLong("quotas.requests.rate.period")
+              val error = Response(Version.Http11, Status.TooManyRequests)
+              error.setContentString(s"Too many requests. Try again in $seconds seconds.")
               Future(error)
             }
           }
