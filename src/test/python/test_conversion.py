6a203332cb4bda478fd41c296552c2fed8a4cdb0
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2017-04-19 13:31:49 -0500

    1. Renamed timeout variable to request_timeout and added a new variable processing_timeout to keep track of extractions and conversions 2. Moved download_file method from conftest.py to test_conversion.py 3. Using processing_timeout in download_file method (earlier this was being used as a decrementing counter)

--- src/test/python/test_conversion.py
+++ src/test/python/test_conversion.py
@@ -6,10 +6,8 @@ import os.path
 import pytest
 import time
 
-from conftest import download_file
 
-
-def test_get_convert(host, api_token, timeout, conversion_data):
+def test_get_convert(host, api_token, request_timeout, processing_timeout, conversion_data):
     # should this test be skipped
     if 'skip' in conversion_data:
         pytest.skip(conversion_data['skip'])
@@ -18,11 +16,12 @@ def test_get_convert(host, api_token, timeout, conversion_data):
     print "Converting from :", conversion_data['file_url']
     print "Converting to   :", conversion_data['output_type']
 
+    stoptime = time.time() + processing_timeout
     endpoint = host + '/dap'
     input_filename = conversion_data['file_url']
     output = conversion_data['output_type']
     output_path = '/tmp/' + str(0) + '_' + splitext(basename(input_filename))[0] + '.' + output
-    r = convert_by_url(endpoint, api_token, input_filename, output, int(timeout))
+    r = convert_by_url(endpoint, api_token, input_filename, output, request_timeout)
     if r.status_code == 200:
         print "Output path     :", output_path
         print "File url        :", r.text
@@ -30,7 +29,7 @@ def test_get_convert(host, api_token, timeout, conversion_data):
             output_filename = output_path
         else:
             output_filename = output_path + basename(r.text)
-        filename = download_file(r.text, output_filename, api_token, 90)
+        filename = download_file(r.text, output_filename, api_token, stoptime)
         assert os.path.isfile(filename), "File was not downloaded"
         print "Downloaded      :", filename
         try:
@@ -43,9 +42,30 @@ def test_get_convert(host, api_token, timeout, conversion_data):
         assert False, "Error retrieving file : %d - %s" % (r.status_code, r.text)
 
 
-def convert_by_url(endpoint, api_token, input_filename, output, timeout):
+def convert_by_url(endpoint, api_token, input_filename, output, request_timeout):
     """Pass file to Polyglot Steward."""
     headers = {'Authorization': api_token, 'Accept': 'text/plain'}
     api_call = endpoint + '/convert/' + output + '/' + urllib.quote_plus(input_filename)
     print "API Call        :", api_call
-    return requests.get(api_call, headers=headers, timeout=timeout)
+    return requests.get(api_call, headers=headers, timeout=request_timeout)
+
+
+def download_file(url, filename, api_token, stoptime):
+    """Download file at given URL"""
+    if not filename:
+        filename = url.split('/')[-1]
+    try:
+        headers = {'Authorization': api_token}
+        r = requests.get(url, headers=headers, stream=True)
+        while (stoptime > time.time() and r.status_code == 404):
+            time.sleep(1)
+            r = requests.get(url, headers=headers, stream=True)
+        if (r.status_code != 404):
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  # Filter out keep-alive new chunks
+                        f.write(chunk)
+                        f.flush()
+    except:
+        raise
+    return filename
