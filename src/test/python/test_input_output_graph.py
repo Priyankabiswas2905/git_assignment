3243d72b8d28c151f4cf868024cab28e738ca124
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2017-04-21 09:43:35 -0500

    Added fix for timeout issue with test_get_input

--- src/test/python/test_input_output_graph.py
+++ src/test/python/test_input_output_graph.py
@@ -11,11 +11,11 @@ def test_get_outputs(host, api_token, request_timeout):
     assert r.status_code == 200, "Did not receive 200, need better test"
 
 
-#@pytest.mark.skip(reason="too slow")
-def test_get_inputs(host, api_token, request_timeout):
+# Special case: This endpoint is slow and does not complete within request_timeout and hence uses processing_timeout.
+def test_get_inputs(host, api_token, processing_timeout):
     url = host + '/dap/inputs'
     print("GET " + url)
-    r = requests.get(url, headers={'Authorization': api_token, 'Accept': 'text/plain'}, timeout=request_timeout)
+    r = requests.get(url, headers={'Authorization': api_token, 'Accept': 'text/plain'}, timeout=processing_timeout)
     r.raise_for_status()
     print(r.text)
     assert r.status_code == 200, "Did not receive 200, need better test"
